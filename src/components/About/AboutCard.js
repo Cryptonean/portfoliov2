b9d63e577ef4bb06fe6205057dde45c29049d85b
Cryptonean <faizanvit8@gmail.com>
2025-01-08 17:22:39 +0530

    aboutcard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,31 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Faizan Shaikh </span>
+            from <span className="purple">Pune, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a blockchain developer and a core contributor at
+            Cardano SPOT.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am an object-oriented programmer, functional programmer, and
+            blockchain enthusiast.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
+
           <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
             </li>
+
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Exploring decentralized governance and
+              innovations
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Faizan Shaikh</footer>
         </blockquote>
       </Card.Body>
     </Card>
